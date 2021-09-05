6e264638aa9fe900c4332740409d47160ad3d593
lgdhades <hades987456@outlook.com>
2021-09-05 18:18:00 +0200

    feat: add styles for Answer Section

---
+++ src/Components/Answer/Answer.css
@@ -0,0 +1,132 @@
+.answer-section-wrapper {
+  height: 17rem;
+  padding: 0px 100px;
+}
+
+@media (max-width: 1024px) {
+  .answer-section-wrapper {
+    padding: 20px 100px;
+    height: 26rem;
+  }
+}
+
+@media (max-width: 768px) {
+  .answer-section-wrapper {
+    padding: 20px 30px;
+    height: 30rem;
+  }
+}
+
+.answer-buttons-wrapper {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+@media (max-width: 480px) {
+  .answer-buttons-wrapper {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+.answer-button,
+.green-button,
+.red-button {
+  padding: 10px;
+  border-radius: 5px;
+  background-color: rgb(187, 179, 179);
+  font-weight: 500;
+  font-size: 18px;
+  margin-bottom: 20px;
+  border: 1px solid black;
+  width: 45%;
+  height: fit-content;
+}
+
+@media (max-width: 1024px) {
+  .answer-button,
+  .green-button,
+  .red-button {
+    width: 100%;
+  }
+}
+
+.green-button {
+  background-color: green;
+  color: white;
+}
+
+.red-button {
+  background-color: red;
+  color: white;
+}
+
+.answer-result {
+  font-size: 25px;
+  font-weight: 700;
+  text-align: center;
+}
+
+.next-question-button-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.next-question-button {
+  padding: 10px 30px;
+  border-radius: 5px;
+  background-color: rgb(187, 179, 179);
+  font-weight: 500;
+  font-size: 18px;
+  border: 1px solid black;
+}
+
+@media (max-width: 768px) {
+  .next-question-button {
+    height: 100px;
+    padding: 10px 20px;
+  }
+}
+
+.finish-message {
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  width: 55%;
+  padding: 50px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  height: fit-content;
+  background-color: white;
+  font-size: 40px;
+  border: 5px solid black;
+  border-radius: 10px;
+  line-height: 1.3;
+  max-width: 800px;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+}
+
+@media (max-width: 768px) {
+  .finish-message {
+    font-size: 25px;
+    padding: 10px 20px;
+  }
+}
+
+.finish-message-overlay {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  z-index: 100;
+}
