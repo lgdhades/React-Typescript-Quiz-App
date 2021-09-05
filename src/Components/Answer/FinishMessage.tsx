2afdbd5bb181f3051a865fc5a1e0ae1af33dc551
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-09-05 18:05:08 +0200

    feat: build FinishMessage component

---
+++ src/Components/Answer/FinishMessage.tsx
@@ -0,0 +1,14 @@
+import './Answer.css'
+
+export default function FinishMessage() {
+  return (
+    <div className='finish-message'>
+      Test is Over!
+      <br />
+      You can close the browser now.
+      <br />
+      Check your mailbox for final results
+      <br />
+    </div>
+  )
+}
