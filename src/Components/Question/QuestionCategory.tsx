fe2dfbfbd787616f47a5b9abc83811f8fd3a70e8
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-09-05 17:35:13 +0200

    feat: modify QuestionCategory to use the MainContext

--- src/Components/Question/QuestionCategory.tsx
+++ src/Components/Question/QuestionCategory.tsx
@@ -1,10 +1,10 @@
 import { useContext } from 'react'
 import './Question.css'
 import QuestionsData from '../../Data/questions.json'
-import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
+import { MainContext } from '../../Context/MainContext'
 
 export default function QuestionCategory() {
-  const [currentQuestion] = useContext(CurrentQuestionContext)
+  const {currentQuestion} = useContext(MainContext)
 
   return (
     <p className='question-category'>
