6f27e7adf5d6b9ffba8dd6c7306bba26922af23c
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-09-05 17:43:25 +0200

    feat: modify QuestionText to use MainContext

--- src/Components/Question/QuestionText.tsx
+++ src/Components/Question/QuestionText.tsx
@@ -1,10 +1,10 @@
 import { useContext } from 'react'
 import './Question.css'
 import QuestionsData from '../../Data/questions.json'
-import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
+import { MainContext } from '../../Context/MainContext'
 
 export default function QuestionText() {
-  const [currentQuestion] = useContext(CurrentQuestionContext)
+  const { currentQuestion } = useContext(MainContext)
 
   return (
     <p className='question-text'>
