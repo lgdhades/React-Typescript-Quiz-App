6673400c79da239a8db1511865901fdfa696c2e5
lgdhades <hades987456@outlook.com>
2021-09-05 17:39:56 +0200

    feat: modify QuestionDifficulty to use MainContext

--- src/Components/Question/QuestionDifficulty.tsx
+++ src/Components/Question/QuestionDifficulty.tsx
@@ -1,17 +1,17 @@
 import { useContext } from 'react'
 import './Question.css'
 import QuestionsData from '../../Data/questions.json'
-import { CurrentQuestionContext } from '../../Context/CurrentQuestionContext'
+import { MainContext } from '../../Context/MainContext'
 
 export default function QuestionDifficulty() {
-  const [CurrentQuestion] = useContext(CurrentQuestionContext)
+  const {currentQuestion} = useContext(MainContext)
 
   function getNumberOfStars() {
-    if (QuestionsData[CurrentQuestion - 1].difficulty === 'hard') {
+    if (QuestionsData[currentQuestion - 1].difficulty === 'hard') {
       return '★★★'
-    } else if (QuestionsData[CurrentQuestion - 1].difficulty === 'medium') {
+    } else if (QuestionsData[currentQuestion - 1].difficulty === 'medium') {
       return '★★✰'
-    } else if (QuestionsData[CurrentQuestion - 1].difficulty === 'easy') {
+    } else if (QuestionsData[currentQuestion - 1].difficulty === 'easy') {
       return '★✰✰'
     }
   }
