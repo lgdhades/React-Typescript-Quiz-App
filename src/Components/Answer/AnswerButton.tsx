67ef2e05babfc250fe9daa88a7c4a42164c8fdc2
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-09-05 18:44:44 +0200

    feat: add more types to AnswerButton component

--- src/Components/Answer/AnswerButton.tsx
+++ src/Components/Answer/AnswerButton.tsx
@@ -2,6 +2,9 @@ import './Answer.css'
 
 interface PropsType {
   children: string
+  disabled?: boolean
+  onClick?: () => void
+  className?: string
 }
 
 export default function AnswerButton({ children, ...restProps }: PropsType) {
